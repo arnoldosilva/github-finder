2ca1e44bea1ebd66bafffb5e4ce46ee421cb5ffc
Arnoldo Silva <analisi@gmail.com>
2022-02-09 11:14:59 -0300

    add user

--- src/layout/users/UsersResults.jsx
+++ src/layout/users/UsersResults.jsx
@@ -1,28 +1,19 @@
-import React,{useState, useEffect} from 'react'
+import React,{useContext, useEffect} from 'react'
 import Loader from '../assets/Loader'
 import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
+
 
 export default function UsersResults() {
 
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const {users,
+    loading,
+    fetchUsers} = useContext(GithubContext)
 
   useEffect(() => {
     fetchUsers()
   },[])
   
-  const fetchUsers = async () =>{
-    const response = await fetch(`${process.env.REACT_APP_GITHUUB_URL}/users`, {
-      headers:{
-        Authorization: `token ${process.env.REACT_APP_GITHUUB_TOKEN}`
-      }
-    }) 
-
-    const data = await response.json()
-    setUsers(data)
-    setLoading(false)
-
-  }
 
   if (!loading) {
     return (
@@ -33,6 +24,6 @@ export default function UsersResults() {
       </div>
     )
   }else{
-    return <Loader />
+    return <Loader color='#e6e6e6' loading={loading} size={150}/>
   }
 }
