21e31c3f5ce1a77f70e1a9bb6004a7dced22cf6a
Arnoldo Silva <analisi@gmail.com>
2022-02-10 06:55:23 -0300

    alert reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -43,11 +43,17 @@ export const GithubProvider = ({ children }) => {
     type: 'SET_LOADING',
   })
 
+  const clearUsers = () => dispatch({
+    type: 'CLEAR_USERS',
+  })
+
+
 
   return <GithubContext.Provider value={{
     users: state.users,
     loading: state.loading,
-    searchUsers
+    searchUsers,
+    clearUsers
   }}>
     {children}
   </GithubContext.Provider>
--- src/layout/users/UserSearch.jsx
+++ src/layout/users/UserSearch.jsx
@@ -1,16 +1,18 @@
 import React, {useState, useContext} from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
 
 export default function UserSearch() {
   const [text, setText] = useState('')
-  const {users, searchUsers} = useContext(GithubContext)
+  const {users, searchUsers, clearUsers} = useContext(GithubContext)
+  const {setAlert} = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value)
 
   const handleSubmit = (e) =>{
     e.preventDefault()
     if (text === '') {
-      alert('Please enter something')
+      setAlert('Please enter something', 'error')
     }else{
       searchUsers(text)
       setText('')
@@ -36,7 +38,7 @@ export default function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button className="btn btn-ghost btn-lg">Clear</button>
+          <button className="btn btn-ghost btn-lg" onClick={clearUsers}>Clear</button>
         </div>
       )}
     </div>
